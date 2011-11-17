226f8271106f94549e67f9cf984d263d8e4d9e81
Goncalo Dias <goncaloRdias@hotmail.com>
2011-11-17 19:25:22 +0000

    updating...

--- a51/1/todo.c
+++
@@ -1,7 +0,0 @@
-char code tab[] = {‘!’, ‘@’, ‘{’, ‘%’, ‘&’, ‘*’, ‘]’, ‘:’}; 
-
-void convertTable(char xdata array[], int size) 
-{ 
-	for (int i = 0; i < size; i = i + 1) 
-		array[i] = tab[array[i]%8]; 
-} 
